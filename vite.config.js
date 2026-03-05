c61d9f7e811ccb2966cab2f450318ab16c874be4
nascdavii <dnascsv@gmail.com>
2026-03-04 23:23:16 -0300

    fix: add Vite MPA config for Vercel deployment

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        refrigeracao: resolve(__dirname, 'refrigeracao.html'),
+        climatizacao: resolve(__dirname, 'climatizacao.html')
+      }
+    }
+  }
+});
